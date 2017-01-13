3d867afc6bb7d256d47401ae1b51c402de0a2e97
songhaoying <songhaoying@gmail.com>
2017-01-13 11:17:02 +0800

    Signed-off-by: songhaoying <songhaoying@gmail.com>

---
+++ src/main/java/sy/controller/WeixinUserController.java
@@ -0,0 +1,37 @@
+package sy.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import sy.model.HonestWeixinUser;
+import sy.service.HonestWeixinUserServiceI;
+
+@Controller
+@RequestMapping("/weixinUserController")
+public class WeixinUserController {
+	
+	
+	private HonestWeixinUserServiceI honestWeixinUserService;
+	
+	public HonestWeixinUserServiceI getHonestWeixinUserService() {
+		return honestWeixinUserService;
+	}
+	
+	@Autowired
+	public void setHonestWeixinUserService(
+			HonestWeixinUserServiceI honestWeixinUserService) {
+		this.honestWeixinUserService = honestWeixinUserService;
+	}
+
+	@RequestMapping("/showUserImg")
+	public String showUserImg( HttpServletRequest request ){
+		List<HonestWeixinUser> weixinUsers= this.honestWeixinUserService.getAllHonestWeixinUser();
+		request.setAttribute("weixinUsers", weixinUsers);
+		return "showUserImg";
+	}
+}
